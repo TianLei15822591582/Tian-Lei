01718186f8c4830e06f3301273c9446b496b62f0
Tian Lei <15822591582@139.com>
2016-12-17 19:54:55 +0800

    commit tianlei test!11111

---
+++ src/org/eclipse/swt/examples/accessibility/AccessibleActionExample.java
@@ -0,0 +1,14 @@
+package org.eclipse.swt.examples.accessibility;
+
+import java.util.*;
+
+
+public class AccessibleActionExample {
+	static float MARGIN = (float) 20.24 ;
+	
+	 
+	public static void main(String[] args) {
+		System.out.println("action "+ MARGIN + " occurred");
+	}
+	 
+}
